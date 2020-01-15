8b317c42473ac52915cff6bc9ffafe333ab49804
junaidnz97 <junaidnz97@gmail.com>
2020-01-15 19:41:46 +0530

    Created discovery server and discovery service

---
+++ demo/src/main/java/com/springcloudexample/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.springcloudexample.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
